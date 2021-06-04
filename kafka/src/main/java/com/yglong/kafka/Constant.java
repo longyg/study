6ea73307f9e0c4c06b16496f78f5bf034f0e5e71
Long Yonggang <yonggang.long@nokia-sbell.com>
2021-06-04 11:45:07 +0800

    daily practice

--- kafka/src/main/java/com/yglong/kafka/Constant.java
+++ kafka/src/main/java/com/yglong/kafka/Constant.java
@@ -4,7 +4,7 @@ import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 public class Constant {
-    public static final String BROKERS = "172.18.69.91:9092";
+    public static final String BROKERS = "localhost:9092";
     public static final String TOPIC = "new-topic";
     public static final String STRING_SERIALIZER = StringSerializer.class.getName();
     public static final String STRING_DESERIALIZER = StringDeserializer.class.getName();
