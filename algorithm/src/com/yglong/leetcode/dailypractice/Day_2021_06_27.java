cee5ef58fee207c906b8bab9b390a80b2d748416
Long Yonggang <longyg1986@163.com>
2021-06-27 23:58:41 +0800

    day 6-27

--- algorithm/src/com/yglong/leetcode/dailypractice/Day_2021_06_27.java
+++ algorithm/src/com/yglong/leetcode/dailypractice/Day_2021_06_27.java
@@ -1,2 +1,10 @@
-package com.yglong.leetcode.dailypractice;public class Day_2021_6_27 {
+package com.yglong.leetcode.dailypractice;
+
+/**
+ * 909. 蛇梯棋
+ * <p>
+ * <p>
+ * 链接：https://leetcode-cn.com/problems/snakes-and-ladders
+ */
+public class Day_2021_06_27 {
 }
