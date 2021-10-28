a958d000e29b5bfbcde6b2ae7467897a8fa6bf42
Long Yonggang <yonggang.long@nokia-sbell.com>
2021-10-28 10:00:40 +0800

    10-2

---
+++ algorithm/src/com/yglong/leetcode/dailypractice/_2021_10/Day_2021_10_27.java
@@ -0,0 +1,65 @@
+package com.yglong.leetcode.dailypractice._2021_10;
+
+import java.util.*;
+
+/**
+ * 301. 删除无效的括号
+ * <p>
+ * 难度：困难
+ * <p>
+ * 链接：https://leetcode-cn.com/problems/remove-invalid-parentheses/
+ */
+public class Day_2021_10_27 {
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(removeInvalidParentheses("(a)())()").toArray()));
+    }
+
+    /**
+     * 广度优先搜索
+     */
+    public static List<String> removeInvalidParentheses(String s) {
+        List<String> ans = new ArrayList<>();
+        Set<String> currSet = new HashSet<>();
+
+        currSet.add(s);
+        while (true) {
+            for (String str : currSet) {
+                if (isValid(str)) {
+                    ans.add(str);
+                }
+            }
+            if (ans.size() > 0) {
+                return ans;
+            }
+            Set<String> nextSet = new HashSet<>();
+            for (String str : currSet) {
+                for (int i = 0; i < str.length(); i++) {
+                    if (i > 0 && str.charAt(i) == str.charAt(i - 1)) {
+                        continue;
+                    }
+                    if (str.charAt(i) == '(' || str.charAt(i) == ')') {
+                        nextSet.add(str.substring(0, i) + str.substring(i + 1));
+                    }
+                }
+            }
+            currSet = nextSet;
+        }
+    }
+
+    private static boolean isValid(String str) {
+        char[] ss = str.toCharArray();
+        int count = 0;
+        for (char c : ss) {
+            if (c == '(') {
+                count++;
+            } else if (c == ')') {
+                count--;
+                if (count < 0) {
+                    return false;
+                }
+            }
+        }
+        return count == 0;
+    }
+}
---
+++ algorithm/src/com/yglong/leetcode/dailypractice/_2021_10/Day_2021_10_28.java
@@ -0,0 +1,57 @@
+package com.yglong.leetcode.dailypractice._2021_10;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 869. 重新排序得到 2 的幂
+ * <p>
+ * 难度：中等
+ * <p>
+ * 链接：https://leetcode-cn.com/problems/reordered-power-of-2/
+ */
+public class Day_2021_10_28 {
+    public static void main(String[] args) {
+        System.out.println(reorderedPowerOf2(16));
+        System.out.println(reorderedPowerOf2(24));
+    }
+
+    static Map<String, Map<Character, Integer>> all = new HashMap<>();
+
+    static {
+        int power = 0;
+        int num = 1;
+        while (num <= (int) 1e9 + 10) {
+            String s = String.valueOf(num);
+            Map<Character, Integer> m = new HashMap<>();
+            for (char c : s.toCharArray()) {
+                m.put(c, m.getOrDefault(c, 0) + 1);
+            }
+            all.put(s, m);
+            num = (int) Math.pow(2, power++);
+        }
+    }
+
+    public static boolean reorderedPowerOf2(int n) {
+        String ns = String.valueOf(n);
+        // 保存输入整数的单个数字出现的次数
+        Map<Character, Integer> sMap = new HashMap<>();
+        for (char c : ns.toCharArray()) {
+            sMap.put(c, sMap.getOrDefault(c, 0) + 1);
+        }
+
+        for (String s : all.keySet()) {
+            if (ns.length() != s.length()) continue;
+            if (isMatch(sMap, all.get(s))) return true;
+        }
+        return false;
+    }
+
+    private static boolean isMatch(Map<Character, Integer> sMap, Map<Character, Integer> tMap) {
+        for (Map.Entry<Character, Integer> entry : sMap.entrySet()) {
+            if (!tMap.containsKey(entry.getKey())) return false;
+            if (!tMap.get(entry.getKey()).equals(entry.getValue())) return false;
+        }
+        return true;
+    }
+}
