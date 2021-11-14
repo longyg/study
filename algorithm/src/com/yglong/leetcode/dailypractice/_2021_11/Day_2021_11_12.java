93d7a4774e4984caa4501719054d1c48c26af8a9
Long Yonggang <yonggang.long@nokia-sbell.com>
2021-11-14 13:43:11 +0800

    day 11-14

---
+++ algorithm/src/com/yglong/leetcode/dailypractice/_2021_11/Day_2021_11_12.java
@@ -0,0 +1,35 @@
+package com.yglong.leetcode.dailypractice._2021_11;
+
+/**
+ * 375. 猜数字大小 II
+ * <p>
+ * 难度：中等
+ * <p>
+ * 链接：https://leetcode-cn.com/problems/guess-number-higher-or-lower-ii/
+ */
+public class Day_2021_11_12 {
+    public static void main(String[] args) {
+        System.out.println(new Day_2021_11_12().getMoneyAmount(10));
+    }
+
+    /**
+     * 递归
+     */
+    int N = 210;
+    int[][] cache = new int[N][N];
+
+    public int getMoneyAmount(int n) {
+        return dfs(1, n);
+    }
+
+    private int dfs(int l, int r) {
+        if (l >= r) return 0;
+        if (cache[l][r] != 0) return cache[l][r];
+        int ans = Integer.MAX_VALUE;
+        for (int i = l; i <= r; i++) {
+            int cur = Math.max(dfs(l, i - 1), dfs(i + 1, r)) + i;
+            ans = Math.min(ans, cur);
+        }
+        return ans;
+    }
+}
---
+++ algorithm/src/com/yglong/leetcode/dailypractice/_2021_11/Day_2021_11_13.java
@@ -0,0 +1,31 @@
+package com.yglong.leetcode.dailypractice._2021_11;
+
+/**
+ * 520. 检测大写字母
+ * <p>
+ * 难度：简单
+ * <p>
+ * 链接：https://leetcode-cn.com/problems/detect-capital/
+ */
+public class Day_2021_11_13 {
+    public static void main(String[] args) {
+        System.out.println(detectCapitalUse("USA"));
+        System.out.println(detectCapitalUse("Google"));
+        System.out.println(detectCapitalUse("hello"));
+        System.out.println(detectCapitalUse("wOrld"));
+        System.out.println(detectCapitalUse("FlaG"));
+    }
+
+    public static boolean detectCapitalUse(String word) {
+        int n = word.length();
+        int c = -1;
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            if (word.charAt(i) >= 'A' && word.charAt(i) <= 'Z') {
+                c = i;
+                cnt++;
+            }
+        }
+        return c == -1 || c == 0 || cnt == n;
+    }
+}
